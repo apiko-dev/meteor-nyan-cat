18e730534028ef59a19ded57193b52ae7f0e93f1
uzumaxy <uzumaxy@gmail.com>
2015-06-14 02:12:01 +0300

    Added sources.

---
+++ package.js
@@ -0,0 +1,12 @@
+Package.describe({
+  name: 'nyan-cat',
+  version: '1.0.0',
+  summary: 'Nyan cat in your console!',
+  git: 'https://github.com/JSSolutions/meteor-nyan-cat',
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.1.0.2');
+  api.addFiles('nyan-cat.js', 'client');
+});
